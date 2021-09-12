5c0a43e60c9ac9af495fd54b82f870b55ca18291
Sheelove Dol <siesel26@gmail.com>
2021-09-11 21:44:47 -0700

    Examples of how to access an object's property values

---
+++ main.js
@@ -0,0 +1,13 @@
+// Creating an object literal
+
+let playStation = {
+  'Disk Drive': 'Blu Ray',
+  'Hard Drive': 'Lightning Fast SSD',
+  Fuel: 'Games'
+};
+
+// One of 2 ways of accessing the value of a property. >>Bracket Notation<<
+console.log(playStation["Disk Drive"]);
+
+// >>Dot Notation<<
+console.log(playStation.Fuel);
