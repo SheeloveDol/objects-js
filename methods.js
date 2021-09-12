888c9f461a1fc136bc2ed42d88f12afd5dcd8aba
Sheelove Dol <siesel26@gmail.com>
2021-09-11 23:28:52 -0700

    Example of nested objects

--- methods.js
+++ methods.js
@@ -15,3 +15,42 @@ let alienShip = {
 
 alienShip.retreat();
 alienShip.takeOff();
+
+
+// Nested Objects
+let spaceship = {
+  passengers: [{janitors: {
+    name: 'Jacob',
+    age: 45
+  }}],
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    'back-up': {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+}; 
+
+// Accessing the first element of the array containing the captain's favorite food
+let capFave = spaceship.crew.captain['favorite foods'][0];
+
+let firstPassenger = spaceship.passengers[0];
