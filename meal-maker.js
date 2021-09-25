5ef3c5f0f7b2ad05e3d7b290a61fdb8b64123b3e
Sheelove Dol <siesel26@gmail.com>
2021-09-25 14:29:54 -0700

    Finally finish with meal maker project

--- meal-maker.js
+++ meal-maker.js
@@ -4,36 +4,74 @@ const menu = {
     mains: [],
     desserts: []
   },
-  get courses() {
-    for (const [key, value] of Object.entries(menu._courses)) {
-      return `${key}: ${value}`;
-    }
-  },
+
   get appetizers() {
-
+    return this._courses.appetizers;
   },
-  set appetizers(appetizersIn) {
-
-  },
-  
   get mains() {
-
+    return this._courses.mains;
   },
-  set mains(mainsIn) {
-
-  },
-
   get desserts() {
-
+    return this._courses.desserts;
   },
-  set desserts(dessertsIn) {
 
+  set appetizers(appetizers) {
+    this._courses.appetizers = appetizers;
+  },
+  set mains(mains) {
+    this._courses.mains = mains;
+  },
+  set desserts(desserts) {
+    this._courses.desserts = desserts;
+  },
+
+  get courses() {
+    return {
+      appetizers: this.appetizers,
+      mains: this.mains,
+      desserts: this.desserts
+    };
   },
 
   addDishToCourse(courseName, dishName, dishPrice) {
-    let dishes = _courses.courseName;
-    let random = Math.floor(Math.random() * dishes.length);
-    return dishes[random];
-  }
+    const dish = {
+      name : dishName,
+      price : dishPrice,
+    };
+    return this._courses[courseName].push(dish)
+  },
+
+  getRandomDishFromCourse(courseName) {
+     const dishes = this._courses[courseName];
+     const random = Math.floor(Math.random() * dishes.length);
+     return dishes[random];
+  },
+
+  generateRandomMeal() {
+    const appetizer =  this.getRandomDishFromCourse('appetizers');
+    const main =  this.getRandomDishFromCourse('mains');
+    const dessert =  this.getRandomDishFromCourse('desserts');
+    const totalPrice = appetizer.price + main.price + dessert.price;
+    return `Your auto-generated meal is ${appetizer.name} with ${main.name} and ${dessert.name} for the total price of $${totalPrice}. Bon appetit!!`;
+    }
 
 }
+
+
+// Adding dishes to courses using the .addDishToCourses() function
+menu.addDishToCourse('appetizers', 'salad', 7.99);
+menu.addDishToCourse('appetizers', 'mango', 4.99);
+menu.addDishToCourse('appetizers', 'eggs', 3.99);
+
+menu.addDishToCourse('mains', 'Burnt Chicken', 16.99);
+menu.addDishToCourse('mains', 'Roasted Duck', 23.99);
+menu.addDishToCourse('mains', 'Calderetta', 31.99);
+
+menu.addDishToCourse('desserts', 'creme brule', 10.99);
+menu.addDishToCourse('desserts', 'marquis au chocolat', 14.99);
+menu.addDishToCourse('desserts', 'fruit cake', 6.99);
+
+
+const meal = menu.generateRandomMeal();
+console.log(meal
+)
